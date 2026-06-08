dcfdb3e4482016a0351aa19916f161f4382291ca
take <taketo.satoh@gmail.com>
2026-06-08 16:08:15 +0900

    Java完成

--- japan.py
+++ japan.py
@@ -10,14 +10,23 @@ driver.get("https://ja.wikipedia.org/wiki/%E9%83%BD%E9%81%93%E5%BA%9C%E7%9C%8C")
 with open("ジャパン.txt", "r", encoding= "utf-8") as f:
         japan = [line.strip() for line in f]
 
+with open("japan.csv", "w", newline="", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerow(["都道府県", "説明"])
+
 for n in japan:
     link = driver.find_element(By.XPATH, f"//a[text() = '{n}']")
-    link.click()
+    
+    driver.execute_script("arguments[0].scrollIntoView();", link)
+    
+    driver.execute_script("arguments[0].click();", link)
+
     
     time.sleep(2)
 
     p = driver.find_element(By.XPATH, "//table[contains(@class,'infobox')]/following-sibling::p[1]")
-    print(p.text)
+    with open("japan.csv", "a", encoding= "utf-8") as f:
+      writer = csv.writer(f)
+      writer.writerow([n, p.text])
     driver.back()
     time.sleep(2)
-
